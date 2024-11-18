c14d6ee1c4999640a258e3a8215106f90fb26109
Daniel Konar <konarsonarsmonar@gmail.com>
2024-11-18 21:10:57 +0100

    Extract spell data fromatting

---
+++ src/shared_assets/asset_scripts/build_data.py
@@ -0,0 +1,64 @@
+from typing import Dict
+from lookups import level_spellpoint_lookup
+from spell_utils import (
+    generate_filename,
+    get_proj_tex,
+    get_proj_hit,
+    get_sounds,
+    get_special_effect_function,
+    parse_damage,
+    get_description,
+    get_los,
+    get_min_damage,
+    get_max_damage,
+    get_damage_roll,
+    get_min_duration,
+    get_max_duration,
+    get_duration_roll,
+    parse_duration,
+    parse_range,
+    get_range,
+    get_traits,
+    get_targets,
+    get_aoe,
+    get_attributes,
+    get_target_type
+)
+
+
+def build_data(row: Dict[str, str]) -> Dict[str, str]: 
+    damage = parse_damage(row['damage'])
+    duration = parse_duration(row['duration'])
+    range = parse_range(row['range'])
+
+    return {
+        'name': row['name'],
+        'target_type': get_target_type(row),
+        'base_cost': row['base_cost'],
+        'usable_in_camp': 'true' if row['usable_in_camp'] == '1' else 'false',
+        'usable_in_combat': 'true' if row['usable_in_combat'] == '1' else 'false',
+        'description': get_description(row),
+        'resist_adjust': row['resist_adjust'],
+        'can_rotate': 'true' if row['can_rotate'] == '1' else 'false',
+        'range': get_range(range),
+        'tags': [],
+        'schools': [row['caster_class']],
+        'proj_tex': get_proj_tex(row['cast_media']),
+        'proj_hit': get_proj_hit(row['resolution_media']),
+        'sounds': get_sounds(row['cast_media'], row['resolution_media']),
+        'special_effect_function': get_special_effect_function(row),
+        'min_damage': get_min_damage(damage),
+        'max_damage': get_max_damage(damage),
+        'damage_roll': get_damage_roll(damage, int(row['effect'])),
+        'min_duration': get_min_duration(duration),
+        'max_duration': get_max_duration(duration),
+        'duration_roll': get_duration_roll(duration),
+        'selection_cost': level_spellpoint_lookup[row['level']],
+        'add_traits_to_target': get_traits(row["effect"]),
+        'is_ray': 'true' if row['target_type'] == '6' else 'false',
+        'is_los': get_los(row),
+        'level': row['level'],
+        'targets': get_targets(row['target_type']),
+        'aoe': get_aoe(row['target_type'], int(row['size'])),
+        'attributes': get_attributes(int(row['effect']))
+    }
